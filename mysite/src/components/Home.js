0566949f9994fee0a59251cce8f21306aa9d7b21
Danny Pifer <piferdg@gmail.com>
2018-10-12 17:10:01 -0600

    Added more code and styling. Added a carousel to the homepage and added more projects to the projects page.

--- mysite/src/components/Home.js
+++ mysite/src/components/Home.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react'
-// import { NavLink } from 'react-router-dom'
+import React from 'react'
+import { UncontrolledCarousel } from 'reactstrap';
 
-class Home extends Component {
-  render() {
-    return(
-      <div className='homepage'>
-        <div className='homepage-name'>
-          <h1>Daniel Pifer</h1>
-          <h4>Full Stack Web Developer</h4>
-        </div>
-      </div>
-    )
+const items = [
+  {
+    src: 'https://lh3.googleusercontent.com/ypTIReM8DMjrCo2f5OvDxM8mADzN3ipG--Mr3LM25KzbJjUAcKw_VrAG1Vcs_WcxadeBNbi0WMtfBSsehSrCyNjrwAg_ihu9sKxKzP3w2x7fAqymYDHwfi6aphiAMsVN6ztxLjLL5DUSTRFcjcUTpKX2uVvCWs9trfKU10Zewk6BYytD_9wbyproVH62MrAW0k6wy2lpmu8FkPZ4-jXf0KfB_m_EHZUV9OYLIy_0ff_DZ33_9K8F5qUsEvZnaT-pDBLCIxgg4qLUxzymlZ6LAxGxBPyb1kwk26sfvU6sZH4ro8OaxxkCkapbWQgQTU4gBOl_qA6_azMjyFBqDdjzhdp3VTx9Q0gpiGrguA6dFhx8w-_QAr2GIEDcfC3-yNhDBVrArLcD9c55kzJpB0-n7sv6IefrFG5FIZy4JOhpSEFyCz7rMmg4NiAjhUhI4FahAP91HvpnTjQhCqaqV2tTMciMzzi7B2dOq40o0L4dgoASxQxuKNvwQG6v-SPLUOXlsj9rveynlbExyexvMpCw6vpG5YJytidqJT0C_PtAiC-MhBxTtyUIagQMnCi-prtpZDrGgA_93Mp-ffqH6ymvKOuzcCsWkLEvPavIfO4JEcqqgya0JFl1sZFlx4AW_8ZJ=w802-h402-no',
+    altText: 'Slide 1',
+    header: 'Fishing',
+    caption: 'Jefferson Lake Rainbow Trout'
+  },
+  {
+    src: 'https://lh3.googleusercontent.com/P2MYPueF1q5C_CGufpIIB4wI5MkUH78SaauFa1aDeg2vAZxPr7ZbGv0C6mMom2CL8erF7S_8gh_RCY6QpNOImJy-56ImguFvTSrVclsuIMSSPlOrknvjjaOgvna7c6a-apSiFTBZSbY9Ueoz50kssaPKZpwkoH8doc83CjPBW-Ca5lRwtJVLLIevNwMMNaegXTB4sRkDni1f4IAWmHfER09jSQnHWKtwmFA0gZO2KxFnBuW-SSeY9C4gMiAWgQjm5OIedsOKmCWk20BwAngyfI_B_7rH8EYMLKX76bh7VFbKetNC03Kvbt5SOGvf7Sv6UzWyYkfu4zAolkuwnNmGSOyWlL7ZiiAo1c1X4shNAaXoGCtbnRLWCBMO_oaUGkblqI8qYnwRHQVTooSJRGZSTgCv3LPARJ7AQyWgD7vDUfciul8Dp-8PclnfiXF3UMFMcp0PZiAGJRrLcMm4Im7bcRj96rz04ZdShHsfLubx2L6ecx2aKiCnl3cGI5rh7Ph776T2nX4utHzSbWaclXfHocSeeE4Zn7-KNCzDxC9QP264JD89twscf9yURjOnWwKyUBYRp4Cri5qk0jovR09Y_L_FxUzsVzYQYJVo6Sha6DMKbuJJTGdFSdiEyCJZ16Pw=w802-h402-no',
+    altText: 'Slide 2',
+    header: 'Hiking',
+    caption: 'Some fall hiking at Jefferson Lake'
+  },
+  {
+    src: 'https://lh3.googleusercontent.com/0f6CJC3NrNFb6hilwncSWyIF00DtRNiWGvCEv9jGLylwER9-poPurQKwtbGduyLjYe3FYRNHPoFbaLwTNEsyYgnuB-iVMXaSFNRDiPjvbKiB1a74Qk4twcFCZVpjyqtaqWhAbRT0vpLThDTA3CAE1LiMJjC9qDDTXJfRjFhRzYhHogXqVJethMMskWa43Mbi6ZWsDIF6Sp36fkUua8BtAfY1Cdu4qV3pjmWH71TM2v0frFtzNR9DKPyAUBwU5AgSLAdL8dDM-ZezhPSpZbrV6NrIE8lViqxh8h3WVAZxh5SMFL3CNy-JV7Q8IcZH2eXK1Ne_i1jrMIELzmONk5FabsazEmFwZmJiQzWjXka6HLHv65uDQvBckGw_dvDtMKMDWupDQ2-dudTyAzEKGEiGrReSgpTRJI9jLl__e0jKpNQ0g5JQsfcoaSJoOmVnYqQF9z8sJMm_S2MU9rSQVgoDEJzQHZTAgLOhAEBCzK94hwPqAKkd5dW1xbVOJascZEE2PEY1jxrWK5IVfINFgtRP0KMb46eR4I0vxuktcHUT8hJ3Xu8Pce2HzeZTYDMu6q4qLESJjnEygocYKRtMtt9zqG13yLKf_vO9JZOgrKMNv-vlhpOrivlTiQjXYKWlXWOe=w802-h402-no',
+    altText: 'Slide 3',
+    header: 'Baseball',
+    caption: 'Taking in a game at AT&T Park in San Francisco'
   }
+];
+
+const Home = () => {
+  return(
+    <div className='homepage'>
+      <div className='homepage-name'>
+        <h1>Daniel Pifer</h1>
+        <h4>Full Stack Web Developer</h4>
+      </div>
+      <div id='carousel'>
+        <UncontrolledCarousel id='carousel-items' items={items} />
+      </div>
+    </div>
+  )
 }
 
 export default Home
--- mysite/src/components/Projects.js
+++ mysite/src/components/Projects.js
@@ -1,96 +1,109 @@
 import React from 'react';
-import sayWhatScreenshot from '../assets/SayWhatScreenshot.png'
+import sayWhatScreenshot from '../assets/sayWhatScreenshot.png'
 import cravScreenshot from '../assets/cravScreenshot.png'
-import starWarsScreenshot from '../assets/StarWarsAppScreenshot.png'
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, a } from 'reactstrap';
+import starWarsScreenshot from '../assets/starWarsPostersScreenshot.png'
+import rockiesRosterScreenshot from '../assets/rockiesRosterScreenshot.png'
+import dogImagesScreenshot from '../assets/dogImagesScreenshot.png'
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 const Projects = () => {
-  return (
-    <div className='projects-page'>
-      <h1>Projects</h1>
-      <div className='projects-container'>
-        <div className='card-one'>
-          <Card id='card-one'>
-            <CardBody>
-              <CardTitle>"SayWhat?" Translation and Travel</CardTitle>
-              <CardSubtitle>Galvanize g90 Capstone</CardSubtitle>
-            </CardBody>
-            <CardImg top width="100%" src={sayWhatScreenshot} alt="SayWhat? Screenshot" />
-            <CardBody>
-              <CardText>The ‘SayWhat?’ app will allow the traveler to translate any words, sentences that they want.</CardText>
-              <a target='_blank' rel="noopener noreferrer" href="https://saywhat-37028.firebaseapp.com/">Launch App</a>
-            </CardBody>
-          </Card>
-        </div>
-        <div className='card-two'>
-          <Card id='card-two'>
-            <CardBody>
-              <CardTitle>"Crāv" - Restaurant App</CardTitle>
-              <CardSubtitle>Collaboration Project</CardSubtitle>
-            </CardBody>
-            <CardImg top width="100%" src={cravScreenshot} alt="Crav Screenshot" />
-            <CardBody>
-              <CardText>'Crāv' is a restaurant selector app, focusing on git workflow and learning Vue.</CardText>
-              <a target='_blank' rel="noopener noreferrer" href="https://crav-5afdb.firebaseapp.com/">Launch App</a>
-            </CardBody>
-          </Card>
-        </div>
-        <div className='card-three'>
-          <Card id='card-three'>
-            <CardBody>
-              <CardTitle>Star Wars App</CardTitle>
-              <CardSubtitle>Movie Poster Generator</CardSubtitle>
-            </CardBody>
-            <CardImg top width="100%" src={starWarsScreenshot} alt="Star Wars Screenshot" />
-            <CardBody>
-              <CardText>The 'Star Wars App' allows the user to click on a movie title, and will then return the poster for that movie.</CardText>
-              <a target='_blank' rel="noopener noreferrer" href="https://star-wars-posters.firebaseapp.com/index.html">Launch App</a>
-            </CardBody>
-          </Card>
-        </div>
-        <div className='card-four'>
-          <Card id='card-four'>
-            <CardBody>
-              <CardTitle>Lorem ipsum dolor</CardTitle>
-              <CardSubtitle>Lorem ipsum dolor</CardSubtitle>
-            </CardBody>
-            <CardImg top width="100%" src={starWarsScreenshot} alt="Star Wars Screenshot" />
-            <CardBody>
-              <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu lectus et metus interdum semper. Integer rhoncus at felis in venenatis. Etiam dapibus vel ante in pharetra.</CardText>
-              <a target='_blank' rel="noopener noreferrer" href="https://star-wars-posters.firebaseapp.com/index.html">Launch App</a>
-            </CardBody>
-          </Card>
-        </div>
-        <div className='card-five'>
-          <Card id='card-five'>
-            <CardBody>
-              <CardTitle>Lorem ipsum dolor</CardTitle>
-              <CardSubtitle>Lorem ipsum dolor</CardSubtitle>
-            </CardBody>
-            <CardImg top width="100%" src={starWarsScreenshot} alt="Star Wars Screenshot" />
-            <CardBody>
-              <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu lectus et metus interdum semper. Integer rhoncus at felis in venenatis. Etiam dapibus vel ante in pharetra.</CardText>
-              <a target='_blank' rel="noopener noreferrer" href="https://star-wars-posters.firebaseapp.com/index.html">Launch App</a>
-            </CardBody>
-          </Card>
-        </div>
-        <div className='card-six'>
-          <Card id='card-six'>
-            <CardBody>
-              <CardTitle>Lorem ipsum dolor</CardTitle>
-              <CardSubtitle>Lorem ipsum dolor</CardSubtitle>
-            </CardBody>
-            <CardImg top width="100%" src={starWarsScreenshot} alt="Star Wars Screenshot" />
-            <CardBody>
-              <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu lectus et metus interdum semper. Integer rhoncus at felis in venenatis. Etiam dapibus vel ante in pharetra.</CardText>
-              <a target='_blank' rel="noopener noreferrer" href="https://star-wars-posters.firebaseapp.com/index.html">Launch App</a>
-            </CardBody>
-          </Card>
+    return (
+      <div className='projects-page'>
+        <h1>Projects</h1>
+        <div className='projects-container'>
+          <div className='card-one'>
+            <Card id='card-one'>
+              <CardBody>
+                <CardTitle>"SayWhat?" Translation and Travel</CardTitle>
+                <CardSubtitle>Galvanize g90 Capstone</CardSubtitle>
+              </CardBody>
+              <CardImg id='screenshot' top width="100%" src={sayWhatScreenshot} alt="SayWhat Screenshot" />
+              <CardBody>
+                <CardText>The ‘SayWhat?’ app will allow the traveler to translate any words, sentences that they want.</CardText>
+                <div className='card-buttons'>
+                  <div className='launch-app-link'>
+                    <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://saywhat-37028.firebaseapp.com/">Launch App</Button>
+                  </div>
+                  <div className='info-button'>
+                  </div>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='card-two'>
+            <Card id='card-two'>
+              <CardBody>
+                <CardTitle>"Crāv" - Restaurant App</CardTitle>
+                <CardSubtitle>Collaboration Project</CardSubtitle>
+              </CardBody>
+              <CardImg id='screenshot' top width="100%" src={cravScreenshot} alt="Crav Screenshot" />
+              <CardBody>
+                <CardText>'Crāv' is a restaurant selector app, focusing on git workflow and learning Vue.</CardText>
+                <div className='card-buttons'>
+                  <div className='launch-app-link'>
+                    <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://crav-5afdb.firebaseapp.com/">Launch App</Button>
+                  </div>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='card-three'>
+            <Card id='card-three'>
+              <CardBody>
+                <CardTitle>Star Wars App</CardTitle>
+                <CardSubtitle>Movie Poster Generator</CardSubtitle>
+              </CardBody>
+              <CardImg id='screenshot' top width="100%" src={starWarsScreenshot} alt="Star Wars Screenshot" />
+              <CardBody>
+                <CardText>The 'Star Wars App' allows the user to click on a movie title, and will then return the poster for that movie.</CardText>
+                <div className='card-buttons'>
+                  <div className='launch-app-link'>
+                    <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://star-wars-posters.firebaseapp.com/index.html">Launch App</Button>
+                  </div>
+                </div>            
+              </CardBody>
+            </Card>
+          </div>
+          <div className='card-four'>
+            <Card id='card-four'>
+              <CardBody>
+                <CardTitle>Colorado Rockies Roster</CardTitle>
+                <CardSubtitle>Add/Delete Players from the roster</CardSubtitle>
+              </CardBody>
+              <CardImg id='screenshot' top width="100%" src={rockiesRosterScreenshot} alt="Rockies Roster Screenshot" />
+              <CardBody>
+                <CardText>The purpose of this app was to practice React and building databases while having some fun at the same time!</CardText>
+                <div className='card-buttons'>
+                  <div className='launch-app-link'>
+                    <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://rockies-roster.firebaseapp.com/">Launch App</Button>
+                  </div>
+                </div>            
+              </CardBody>
+            </Card>
+          </div>
+          <div className='card-five'>
+            <Card id='card-five'>
+              <CardBody>
+                <CardTitle>The Good Mood App!</CardTitle>
+                <CardSubtitle>Click the bone to see a new dog</CardSubtitle>
+              </CardBody>
+              <CardImg id='screenshot' top width="100%" src={dogImagesScreenshot} alt="Dog Images Screenshot" />
+              <CardBody>
+                <CardText>The 'Good Mood App' was another fun app that I made to practice using React with an API.</CardText>
+                <div className='card-buttons'>
+                  <div className='launch-app-link'>
+                    <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://dog-app-e92c3.firebaseapp.com/">Launch App</Button>
+                  </div>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    )
+  }
 
 export default Projects;
