93f00a9ebc9b1ebcad32ab50d5d3eb8c4f6ebb3f
Danny Pifer <piferdg@gmail.com>
2018-10-17 19:34:17 -0600

    Adding image files to home page

--- mysite/src/components/Home.js
+++ mysite/src/components/Home.js
@@ -3,19 +3,19 @@ import { UncontrolledCarousel } from 'reactstrap';
 
 const items = [
   {
-    src: 'https://lh3.googleusercontent.com/kGoHTNdNBKeC5UEYuVPu_NdjOlcBlcGxKfK-hi9q7ZSo7MytfFFaQQaM94uQbh6XYGBMtoKP7JFUcRMUIBHM_-1dHue60aKVhCQqLrRpm_jeQ6gK_I0m2RKbyYUAm7ZSi-0aWvAJ_oYOQcCscMErzx3xrBSwttl7pPefQSIfPyub2fh5BuH7lz5WvwGM_ODdfzesgcqZ6OSP_CiXPl1RQzDLkOGSxfGm9wIGWMRWFQwqGZL3V7ewaY-vTG__4F3WW9nGNtzAHiYJjUVtqOjJs-cDRht06G3z-owixALz4Y7gqgxzGRvzdByvOvMa4xEGrJpqhJ5slZCca8E7W9Z1ZEuKjdhHgF1oxDNSSA4fZ1Vr6dpjSTLJH7E0DTysqYIYw1DaFNvqkS07FatR6kvS9_kHt8GCyzEatF24ShlG5Y-ZbKXiLtbq3ufBj32AixYWgrnqhhqNiWPYxpOkS59lN0o4TmWg2Mxll9nm58MOzpcvi156N7RxpbqINXOXXc5C5FfAn8B4QKGefIJuMHCCSiDEiMj_ojxRbQsgBvawqIl-kfFsuzd8EgrOMPo5l2OkJlib1aUeJoMnkVx6JitJyO84PUxJZnTTMX1ipeAbJOmYA1cW3j_Zg7z3iSkD3ZsC=w802-h403-no',
+    src: 'https://image.ibb.co/k2gw5f/fishing.png',
     altText: 'Slide 1',
     header: 'Fishing',
     caption: 'Jefferson Lake Rainbow Trout'
   },
   {
-    src: 'https://lh3.googleusercontent.com/P2MYPueF1q5C_CGufpIIB4wI5MkUH78SaauFa1aDeg2vAZxPr7ZbGv0C6mMom2CL8erF7S_8gh_RCY6QpNOImJy-56ImguFvTSrVclsuIMSSPlOrknvjjaOgvna7c6a-apSiFTBZSbY9Ueoz50kssaPKZpwkoH8doc83CjPBW-Ca5lRwtJVLLIevNwMMNaegXTB4sRkDni1f4IAWmHfER09jSQnHWKtwmFA0gZO2KxFnBuW-SSeY9C4gMiAWgQjm5OIedsOKmCWk20BwAngyfI_B_7rH8EYMLKX76bh7VFbKetNC03Kvbt5SOGvf7Sv6UzWyYkfu4zAolkuwnNmGSOyWlL7ZiiAo1c1X4shNAaXoGCtbnRLWCBMO_oaUGkblqI8qYnwRHQVTooSJRGZSTgCv3LPARJ7AQyWgD7vDUfciul8Dp-8PclnfiXF3UMFMcp0PZiAGJRrLcMm4Im7bcRj96rz04ZdShHsfLubx2L6ecx2aKiCnl3cGI5rh7Ph776T2nX4utHzSbWaclXfHocSeeE4Zn7-KNCzDxC9QP264JD89twscf9yURjOnWwKyUBYRp4Cri5qk0jovR09Y_L_FxUzsVzYQYJVo6Sha6DMKbuJJTGdFSdiEyCJZ16Pw=w802-h402-no',
+    src: 'https://image.ibb.co/dfLvy0/me-hiking.png',
     altText: 'Slide 2',
     header: 'Hiking',
     caption: 'Some fall hiking at Jefferson Lake'
   },
   {
-    src: 'https://lh3.googleusercontent.com/4fAtsAAINi9rySLESglBV0G8YJOJoOo8j4E4j92BR3To0gF3goH2iCKcIdLA45gyqoOy6Ku36wNAj72m83PVcCYzMHwkAkQDDw8BbS1JBeGLAm7DzrblzAumLEDRqxN9JIzbvqpYQjQdGH-wy44gz4PQAkm-RVvE0qiH-J7hZpx4Mxc_X3HiPtKAFVpQiYRcw46ytg_HwrIF53x7RtoLo2vHGts6AlaepciF9HhJfk4Oz8RW2dwQ_GXhNR6p1pFqiOnmsQA_PxdnRg4QUewjqD4dpvldrG9_KZvbpqLrv0CS6wwoUr-i21Ged5ib_P8VNKyusLigGEo9xYRKJyePbtcx4WWSuFFly-uWrTgO7wjYR-3xunCmoqiyJpGykN0NOkbno3Q1O2oKzb5g24Y1Gl7Z95m9wlMeMWJ0qvh-a35kkz4Ryr99XoqMk29tUDxEFMLN7GxUIK8oBb4X8s_VMKbPIcUrTc9htatmTWFkQPmRvtUCIOw3f7zOmWuS8ZW4UGkTWKbly1Cs0swg-v7_gb__UkuDeHI_cm2G6X-6IRqEHZtrXQnh-Sm93VbYimlDFsipqrCRtB54xlE09bPke5ecsLpAESncfSYEkTeclL9oLCiccWBcqkWp_YdKXZTf=w803-h402-no',
+    src: 'https://image.ibb.co/d1PoJ0/baseball.png',
     altText: 'Slide 3',
     header: 'Baseball',
     caption: 'Taking in a game at AT&T Park in San Francisco'
