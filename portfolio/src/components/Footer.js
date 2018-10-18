9fe09e293eb2a112e17cb9c5e6ffa6b87b1a939a
Danny Pifer <piferdg@gmail.com>
2018-10-18 08:22:00 -0600

    Adding new react app

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return(
+    <div className='footer'>
+      <p>Daniel Pifer - All Rights Reserved &copy;2018</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-name'>
+        <a href='/'>Home</a>
+      </div>
+      <div className='header-projects'> 
+        <NavLink to='/projects'>Projects</NavLink>       
+      </div>
+      <div className='header-github'>
+        <a target='-blank' href='https://github.com/piferdg'>GitHub</a>
+      </div>
+      <div className='header-linkedin'>
+        <a target='-blank' href='https://www.linkedin.com/in/daniel-pifer/'>Linkedin</a>
+      </div>
+      <div className='header-contact'>   
+        <NavLink to='/contact'>Contact</NavLink>        
+      </div>
+    </div>
+  )
+}
+
+export default Header
