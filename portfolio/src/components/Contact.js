57b50e1f79d7af6afdb22f085450dd2e7f4cd1b6
Danny Pifer <piferdg@gmail.com>
2018-10-22 13:17:16 -0600

    Added downloadable link to my resume on contact page and restyled that page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import me from '../assets/meMtEvansHiking.png'
+import PDF from '../assets/DanielPifer_DeveloperResume_forDownload.pdf'
 
 const Contact = () => {
   return (
@@ -21,14 +22,19 @@ const Contact = () => {
       </div>
       <div className='contact-main'>
         <div className='contact-info'>
-          <h3>Email: <a href='mailto:piferdg@gmail.com'>piferdg@gmail.com</a></h3>
           <h3>Phone: &#40;303&#41; 847-9394</h3>
           <h3>Location: Denver, CO.</h3>
           <div className='contact-github-link'>
-            <h3>GitHub: <a target='-blank' href='https://github.com/piferdg'>/piferdg</a></h3>
+            <h3>GitHub: <a target='-blank' href='https://github.com/piferdg'>piferdg</a></h3>
           </div>
           <div className='contact-linkedin-link'>
-            <h3>Linkedin: <a target='-blank' href='https://www.linkedin.com/in/daniel-pifer/'>/daniel-pifer</a></h3>
+            <h3>Linkedin: <a target='-blank' href='https://www.linkedin.com/in/daniel-pifer/'>daniel-pifer</a></h3>
+          </div>
+          <div className='contact-email-link'>
+            <h3>Email: <a href='mailto:piferdg@gmail.com'>piferdg@gmail.com</a></h3>
+          </div>
+          <div className='contact-resume-link'>
+            <h3>Resume: <a href={PDF} download>Download my resume!</a></h3>
           </div>
         </div>
         <div className='contact-image'>
