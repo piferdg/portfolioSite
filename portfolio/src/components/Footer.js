eea1efa9b25885bebabc2b705434baf0562a87d0
Danny Pifer <piferdg@gmail.com>
2018-10-30 17:12:19 -0600

    Added some styling to the projects page. Added some shadows to the buttons, project cards, contact image, and carousel. Also removed overscroll.

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -3,7 +3,9 @@ import React from "react"
 const Footer = () => {
   return(
     <div className='footer'>
-      <p>Daniel Pifer - All Rights Reserved &copy;2018</p>
+      <div className='footer-text'>
+        <p>Daniel Pifer - All Rights Reserved &copy;2018</p>
+      </div>
     </div>
   )
 }
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -29,27 +29,23 @@ const Projects = () => {
         <div className='card-one'>
           <Card id='card-one'>
             <CardBody>
-              <CardTitle>"SayWhat?" Translation and Travel</CardTitle>
+              <CardTitle className='project-title'>"SayWhat?" Translation and Travel</CardTitle>
               <CardSubtitle>Galvanize g90 Capstone</CardSubtitle>
             </CardBody>
             <CardImg id='screenshot' top width="100%" src={sayWhatScreenshot} alt="SayWhat Screenshot" />
             <CardBody>
               <CardText>The ‘SayWhat?’ app will allow the traveler to translate any words, sentences that they want.</CardText>
-              <CardText>Technologies Used: React, Reactstrap, CSS, Firebase, Google Cloud Machine Learning API, PostgreSQL, Node, Express, Knex, Heroku</CardText>
+              <CardText><u>Technologies Used:</u> React, Reactstrap, CSS, Firebase, Google Cloud Machine Learning API, PostgreSQL, Node, Express, Knex, Heroku</CardText>
               <div className='card-buttons'>
                 <div className='launch-app-link'>
-                  <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://saywhat-37028.firebaseapp.com/">Launch App</Button>
+                  <Button className='launch-app-button' color='primary' target='_blank' rel="noopener noreferrer" href="https://saywhat-37028.firebaseapp.com/">Launch App</Button>
                 </div>
                 <div className='capstone-video'>
-                  <Button color="success" id="toggler">
+                  <Button className='capstone-video-button' color="success" id="toggler">
                     Capstone Presentation
                   </Button>
                   <UncontrolledCollapse toggler="#toggler">
-                    <Card className='video-card'>
-                      <CardBody>
-                        <iframe title='Capstone Presentation' width="570" height="300" src="https://www.youtube.com/embed/MQUKROvyDsA" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                      </CardBody>
-                    </Card>
+                    <iframe className='video-card' title='Capstone Presentation' src="https://www.youtube.com/embed/MQUKROvyDsA" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                   </UncontrolledCollapse>
                 </div>
               </div>
@@ -59,16 +55,16 @@ const Projects = () => {
         <div className='card-two'>
           <Card id='card-two'>
             <CardBody>
-              <CardTitle>"Crāv" - Restaurant App</CardTitle>
+              <CardTitle className='project-title'>"Crāv" - Restaurant App</CardTitle>
               <CardSubtitle>Collaboration Project</CardSubtitle>
             </CardBody>
             <CardImg id='screenshot' top width="100%" src={cravScreenshot} alt="Crav Screenshot" />
             <CardBody>
               <CardText>'Crāv' is a restaurant selector app, focusing on git workflow and learning Vue.</CardText>
-              <CardText>Technologies Used: Vue, CSS, firebase, PostgreSQL, Node, Express, Knex, Heroku</CardText>
+              <CardText><u>Technologies Used:</u> Vue, CSS, firebase, PostgreSQL, Node, Express, Knex, Heroku</CardText>
               <div className='card-buttons'>
                 <div className='launch-app-link'>
-                  <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://crav-5afdb.firebaseapp.com/">Launch App</Button>
+                  <Button className='launch-app-button' color='primary' target='_blank' rel="noopener noreferrer" href="https://crav-5afdb.firebaseapp.com/">Launch App</Button>
                 </div>
               </div>
             </CardBody>
@@ -77,16 +73,16 @@ const Projects = () => {
         <div className='card-three'>
           <Card id='card-four'>
             <CardBody>
-              <CardTitle>Colorado Rockies Roster</CardTitle>
+              <CardTitle className='project-title'>Colorado Rockies Roster</CardTitle>
               <CardSubtitle>Add/Delete Players from the roster</CardSubtitle>
             </CardBody>
             <CardImg id='screenshot' top width="100%" src={rockiesRosterScreenshot} alt="Rockies Roster Screenshot" />
             <CardBody>
               <CardText>The purpose of this app was to practice React and building databases while having some fun at the same time!</CardText>
-              <CardText>Technologies Used: React, CSS, firebase, PostgreSQL, Node, Express, Knex, Heroku</CardText>
+              <CardText><u>Technologies Used:</u> React, CSS, firebase, PostgreSQL, Node, Express, Knex, Heroku</CardText>
               <div className='card-buttons'>
                 <div className='launch-app-link'>
-                  <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://rockies-roster.firebaseapp.com/">Launch App</Button>
+                  <Button className='launch-app-button' color='primary' target='_blank' rel="noopener noreferrer" href="https://rockies-roster.firebaseapp.com/">Launch App</Button>
                 </div>
               </div>
             </CardBody>
@@ -95,16 +91,16 @@ const Projects = () => {
         <div className='card-four'>
           <Card id='card-five'>
             <CardBody>
-              <CardTitle>The Good Mood App!</CardTitle>
+              <CardTitle className='project-title'>The Good Mood App!</CardTitle>
               <CardSubtitle>Click the bone to see a new dog</CardSubtitle>
             </CardBody>
             <CardImg id='screenshot' top width="100%" src={dogImagesScreenshot} alt="Dog Images Screenshot" />
             <CardBody>
               <CardText>The 'Good Mood App' was another fun app that I made to practice using React with an API.</CardText>
-              <CardText>Technologies Used: React, CSS, firebase, PostgreSQL, Node, Express, Knex, Heroku</CardText>
+              <CardText><u>Technologies Used:</u> React, CSS, firebase, PostgreSQL, Node, Express, Knex, Heroku</CardText>
               <div className='card-buttons'>
                 <div className='launch-app-link'>
-                  <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://dog-app-e92c3.firebaseapp.com/">Launch App</Button>
+                  <Button className='launch-app-button' color='primary' target='_blank' rel="noopener noreferrer" href="https://dog-app-e92c3.firebaseapp.com/">Launch App</Button>
                 </div>
               </div>
             </CardBody>
@@ -113,16 +109,16 @@ const Projects = () => {
         <div className='card-five'>
           <Card id='card-three'>
             <CardBody>
-              <CardTitle>Star Wars App</CardTitle>
+              <CardTitle className='project-title'>Star Wars App</CardTitle>
               <CardSubtitle>Movie Poster Generator</CardSubtitle>
             </CardBody>
             <CardImg id='screenshot' top width="100%" src={starWarsScreenshot} alt="Star Wars Screenshot" />
             <CardBody>
               <CardText>The 'Star Wars App' allows the user to click on a movie title, and will then return the poster for that movie.</CardText>
-              <CardText>Technologies Used: JavaScript, CSS, firebase, API</CardText>
+              <CardText><u>Technologies Used:</u> JavaScript, CSS, firebase, API</CardText>
               <div className='card-buttons'>
                 <div className='launch-app-link'>
-                  <Button color='primary' target='_blank' rel="noopener noreferrer" href="https://star-wars-posters.firebaseapp.com/index.html">Launch App</Button>
+                  <Button className='launch-app-button' color='primary' target='_blank' rel="noopener noreferrer" href="https://star-wars-posters.firebaseapp.com/index.html">Launch App</Button>
                 </div>
               </div>
             </CardBody>
